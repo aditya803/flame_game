c354d76248231d9e307b23cc4da1b9931d567f92
Aditya Pandey <adityapan25@gmail.com>
2024-05-15 23:56:46 +0530

    score counter

--- lib/game/gift_grab_game.dart
+++ lib/game/gift_grab_game.dart
@@ -1,5 +1,7 @@
-import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
+import 'package:flame/text.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flame_game/components/bg_component.dart';
 import 'package:flame_game/components/gift_component.dart';
@@ -11,6 +13,12 @@ import '../constants/globals.dart';
 
 class GiftGrabGame extends FlameGame with HasCollisionDetection{
   int score =0;
+
+  late Timer _timer;
+  int _remainingTime = 30;
+  late TextComponent _scoreText;
+  late TextComponent _timeText;
+
 
   @override
   Future<void> onLoad() async{
@@ -27,5 +35,51 @@ class GiftGrabGame extends FlameGame with HasCollisionDetection{
     add(IceComponent(startPosition: Vector2(200,200)));
     add(IceComponent(startPosition: Vector2(size.x - 200,size.y - 200)));
     add(ScreenHitbox());
+
+    _scoreText = TextComponent(
+        text: 'Score: $score',
+        position: Vector2(40, 40),
+        anchor: Anchor.topLeft,
+        textRenderer: TextPaint(
+          style: TextStyle(
+            color: BasicPalette.white.color,
+            fontSize: 20
+          )
+        )
+    );
+    add(_scoreText);
+
+    _timeText = TextComponent(
+        text: 'Time: $_remainingTime seconds',
+        position: Vector2(size.x-40, 40),
+        anchor: Anchor.topRight,
+        textRenderer: TextPaint(
+            style: TextStyle(
+                color: BasicPalette.white.color,
+                fontSize: 20
+            )
+        )
+    );
+    add(_timeText);
+
+    _timer = Timer(
+      1,
+      repeat: true,
+      onTick: (){
+        if(_remainingTime == 0){
+          pauseEngine();
+        }else{
+          _remainingTime-=1;
+        }
+      });
+    _timer.start();
+  }
+  
+  @override
+  void update(dt){
+    super.update(dt);
+    _timer.update(dt);
+    _scoreText.text = 'Score: $score';
+    _timeText.text = 'Time: $_remainingTime seconds';
   }
 }
