15c268011d763c5c93e7ea10f55ec6bae44cdb5e
Aditya Pandey <adityapan25@gmail.com>
2024-05-15 19:48:49 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    )
+  );
+}
+
+
